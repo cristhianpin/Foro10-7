d0bff80d4a533a0262be2e3d4d5725198ddbc519
cristhianpin <cristhianpin92@gmail.com>
2023-07-10 23:03:57 -0500

    primer commit

--- ConsoleApp1/Repositories/EstudianteRepository.cs
+++ ConsoleApp1/Repositories/EstudianteRepository.cs
@@ -19,5 +19,69 @@ namespace ConsoleApp1.Repositories
             await _context.SaveChangesAsync();
  
         }
+
+        public static async Task agregarEstudiante()
+        {
+            Console.WriteLine("Metodo agregar estudiante");
+
+            using (SchoolContext context = new SchoolContext())
+            {
+                Student std = new Student();
+                std.Name = "Bill";
+                context.Students.Add(std);
+                await context.SaveChangesAsync();
+
+                Console.WriteLine("Codigo: " + std.StudentId + " Nombre: " + std.Name);
+            }
+        }
+
+        public static async Task consultarEstudiantes()
+        {
+            Console.WriteLine("Metodo consultar estudiantes");
+            using (SchoolContext context = new SchoolContext())
+            {
+                List<Student> listEstudiantes = await context.Students.ToListAsync();
+
+                foreach (var item in listEstudiantes)
+                {
+                    Console.WriteLine("Codigo: " + item.StudentId + " Nombre: " + item.Name);
+                }
+            }
+        }
+
+        public static async Task consultarEstudiante()
+        {
+            Console.WriteLine("Metodo consultar estudiante por id");
+            using (SchoolContext context = new SchoolContext())
+            {
+                Student std = await context.Students.FindAsync(11);
+                Console.WriteLine("Codigo: " + std.StudentId + " Nombre: " + std.Name);
+            }
+        }
+
+        public static async Task modificarEstudiante()
+        {
+            Console.WriteLine("Metodo modificar estudiante");
+            using (SchoolContext context = new SchoolContext())
+            {
+                Student std = await context.Students.FindAsync(11);
+
+                std.Name = "Bill Gates";
+                await context.SaveChangesAsync();
+                Console.WriteLine("Codigo: " + std.StudentId + " Nombre: " + std.Name);
+            }
+        }
+
+        public static async Task eliminarEstudiante()
+        {
+            Console.WriteLine("Metodo eliminar estudiante");
+            using (SchoolContext context = new SchoolContext())
+            {
+                Student std = await context.Students.FindAsync(11);
+                context.Students.Remove(std);
+                await context.SaveChangesAsync();
+                Console.WriteLine("Codigo: " + std.StudentId + " Nombre: " + std.Name);
+            }
+        }
     }
 }
